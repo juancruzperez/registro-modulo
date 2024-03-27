f5b1bb41e9e4e589386881fed78984e8ca72d5f2
Juan Cruz Pérez <juanceprez@gmail.com>
2024-03-27 17:01:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 const uneteBtn = document.getElementById('unete');
+const ingresaBtn = document.getElementById('ingresa');
 
-uneteBtn.addEventListener("click",function(event){
+ingresaBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+})
+
+uneteBtn.addEventListener("click",(event)=>{
     event.preventDefault();
 })
 
